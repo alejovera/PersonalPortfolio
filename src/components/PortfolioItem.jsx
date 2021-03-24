7a8e13d53b7258fd04dd3b47d542a447f6393ea7
Alejo <vera.alejo0903@gmail.com>
2021-03-24 11:14:04 -0300

    Links a proyectos

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -19,11 +19,11 @@ const PortfolioItem = (props) =>{
     return(
         <div>
             <div className={hover ? 'card card-hover' : 'card'} onMouseOver={handleMouseover} onMouseOut={handleMouseout}>
-                <img className="card-img-top img_card" src={sampleImg}/>
+                <img className="card-img-top img_card" src={props.image}/>
                 <div className={hover ? 'card-body card-body_hover' : 'card-body card-body_normal'}>
                     <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
-                    <a href="#" id="portfolio-btn" className={hover ? 'btn btn-especial' : 'btn btn-warning'}>Ver página Web</a>
+                    <a href={props.url} target="_blank" id="portfolio-btn" className={hover ? 'btn btn-especial' : 'btn btn-warning'}>Ver página Web</a>
                 </div>
             </div>
         </div>
